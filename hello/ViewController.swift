bf0bee734c08e44feeadb39087eda68e5735a3ef
ktm <ktm.britney@gmail.com>
2018-04-11 02:18:05 +0300

    Initial Commit

---
+++ hello/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello
+//
+//  Created by Mohammed Abdulaziz on ٢٥ رجب، ١٤٣٩ هـ.
+//  Copyright © ١٤٣٩ هـ Mohammed Abdulaziz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
